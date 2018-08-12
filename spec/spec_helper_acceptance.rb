70f5e17ebf687be91d8b1d5862b885cbb4cdefe9
Tim Meusel <tim@bastelfreak.de>
2018-08-12 18:57:19 +0200

    update spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,34 +1,20 @@
 require 'beaker-rspec'
-require 'pry'
+require 'beaker-puppet'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
-hosts.each do |host|
-  # Install Puppet
-  if host['platform'] =~ %r{sles}
-    host.install_package('puppet')
-    host.install_package('facter')
-  else
-    install_puppet(default_action: 'gem_install')
-  end
-
-  on host, "mkdir -p #{host['distmoduledir']}"
-end
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module
-    puppet_module_install(source: proj_root, module_name: 'virtualbox')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-apt'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'stahnma-epel'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'darin-zypprepo'), acceptable_exit_codes: [0, 1]
+  hosts.each do |host|
+    on host, puppet('module', 'install', 'darin-zypprepo'), acceptable_exit_codes: [0, 1]
+    if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
+      on(host, "sed -i '/nodocs/d' /etc/yum.conf")
     end
   end
 end
