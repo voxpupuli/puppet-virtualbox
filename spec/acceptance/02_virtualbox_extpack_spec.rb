8dfe9cbf5370e26c9cbb4aa3cf08099acfc9f0e8
Tim Meusel <tim@bastelfreak.de>
2018-08-12 18:57:10 +0200

    update acceptance tests

--- spec/acceptance/02_virtualbox_extpack_spec.rb
+++ spec/acceptance/02_virtualbox_extpack_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper_acceptance'
 
-authors = %w[camptocamp puppet]
-
 describe 'virtualbox extpack' do
   let(:install) do
     <<-EOS
@@ -29,58 +27,52 @@ describe 'virtualbox extpack' do
     EOS
   end
 
-  authors.each do |author|
-    context "with #{author}/archive" do
-      before(:all) do
-        hosts.each do |host|
-          authors.each do |author2|
-            on host, puppet('module', 'uninstall', '-f', "#{author2}-archive"), acceptable_exit_codes: [0, 1]
-          end
+  context 'with puppet/archive' do
+    before(:all) do
+      hosts.each do |host|
+        on host, puppet('module', 'install', 'puppet-archive')
+      end
+    end
 
-          on host, puppet('module', 'install', "#{author}-archive")
-        end
+    context 'install extpack' do
+      it 'is idempotent' do
+        apply_manifest(install, catch_failures: true)
+        apply_manifest(install, catch_changes: true)
       end
 
-      context 'install extpack' do
-        it 'is idempotent' do
-          apply_manifest(install, catch_failures: true)
-          apply_manifest(install, catch_changes: true)
-        end
-
-        describe file('/usr/lib/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack') do
-          it { is_expected.to be_directory }
-        end
-
-        describe file('/usr/src/Oracle_VM_VirtualBox_Extension_Pack.tgz') do
-          it { is_expected.to be_file }
-          its(:md5sum) { is_expected.to eq '41f1d66e0be1c183917c95efed89db56' }
-        end
-
-        describe command('VBoxManage list extpacks') do
-          its(:exit_status) { is_expected.to eq 0 }
-          its(:stdout) { is_expected.to match %r{Extension Packs: 1} }
-        end
+      describe file('/usr/lib/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack') do
+        it { is_expected.to be_directory }
       end
 
-      context 'uninstall extpack' do
-        it 'is idempotent' do
-          apply_manifest(install, catch_failures: true)
-          apply_manifest(uninstall, catch_failures: true)
-          apply_manifest(uninstall, catch_changes: true)
-        end
+      describe file('/usr/src/Oracle_VM_VirtualBox_Extension_Pack.tgz') do
+        it { is_expected.to be_file }
+        its(:md5sum) { is_expected.to eq '41f1d66e0be1c183917c95efed89db56' }
+      end
 
-        describe file('/usr/lib/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack') do
-          it { is_expected.not_to be_directory }
-        end
+      describe command('VBoxManage list extpacks') do
+        its(:exit_status) { is_expected.to eq 0 }
+        its(:stdout) { is_expected.to match %r{Extension Packs: 1} }
+      end
+    end
 
-        describe file('/usr/src/Oracle_VM_VirtualBox_Extension_Pack.tgz') do
-          it { is_expected.not_to be_file }
-        end
+    context 'uninstall extpack' do
+      it 'is idempotent' do
+        apply_manifest(install, catch_failures: true)
+        apply_manifest(uninstall, catch_failures: true)
+        apply_manifest(uninstall, catch_changes: true)
+      end
 
-        describe command('VBoxManage list extpacks') do
-          its(:exit_status) { is_expected.to eq 0 }
-          its(:stdout) { is_expected.to match %r{Extension Packs: 0} }
-        end
+      describe file('/usr/lib/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack') do
+        it { is_expected.not_to be_directory }
+      end
+
+      describe file('/usr/src/Oracle_VM_VirtualBox_Extension_Pack.tgz') do
+        it { is_expected.not_to be_file }
+      end
+
+      describe command('VBoxManage list extpacks') do
+        its(:exit_status) { is_expected.to eq 0 }
+        its(:stdout) { is_expected.to match %r{Extension Packs: 0} }
       end
     end
   end
