56e26aae50e7ffd834d52009e60ba1cb15acaef2
Fabien COMBERNOUS <fabien.combernous@adullact.org>
2019-10-04 11:42:21 +0200

    use puppet strings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,53 +1,46 @@
-# == Class: virtualbox
+# @summary Installs VirtualBox.
 #
-# This is a public class. This class installs VirtualBox.
-#
-# === Parameters
-#
-# [*version*]
-#   The major version of the package to install.
-#   Defaults to 5.0
-# [*package_ensure*]
+# @param version The major version of the package to install.
+# @param package_ensure
 #   This gets passed to the package resource as the value of the 'ensure'
 #   parameter. This can be used to specify a package version.
-#   Defaults to present
-# [*manage_repo*]
+# @param manage_repo
 #   Should this module manage the package repository?
 #   Defaults to true
-# [*manage_ext_repo*]
+# @param manage_ext_repo
 #   On applicable platforms, should this module manage the external dependency
 #   repository when `manage_kernel` is set to true?
 #   Defaults to true
-# [*repo_proxy*]
-#   Defaults to undef
-# [*manage_package*]
+# @param repo_proxy proxy used by yum
+# @param manage_package
 #   Should this module manage the package?
 #   Defaults to true
-# [*manage_kernel*]
+# @param manage_kernel
 #   Should this module install the VirtualBox kernel modules?
 #   Defaults to true
-# [*vboxdrv_dependencies*]
+# @param vboxdrv_dependencies
 #   Dependencies for building the VirtualBox kernel modules.
 #   Defaults depend on the platform. See virtualbox::params.
-# [*package_name*]
+# @param package_name
 #   The name of the package to install. This must be the full packge name when
 #   not the default. When the default is in use, it gets compounded with the
 #   major.minor components of the version number.
 #   Defaults to 'VirtualBox' for RedHat and 'virtualbox' for Debian
 #
 class virtualbox (
-  String $version               = $virtualbox::params::version,
-  String $package_ensure        = $virtualbox::params::package_ensure,
+  String $version               = '5.1',
+  String $package_ensure        = 'present',
   String $package_name          = $virtualbox::params::package_name,
   Boolean $manage_repo          = $virtualbox::params::manage_repo,
-  Boolean $manage_ext_repo      = $virtualbox::params::manage_ext_repo,
-  Boolean $manage_package       = $virtualbox::params::manage_package,
-  Boolean $manage_kernel        = $virtualbox::params::manage_kernel,
+  Boolean $manage_ext_repo      = true,
+  Boolean $manage_package       = true,
+  Boolean $manage_kernel        = true,
   Array $vboxdrv_dependencies   = $virtualbox::params::vboxdrv_dependencies,
-  Optional[String] $repo_proxy  = $virtualbox::params::repo_proxy,
+  Optional[String] $repo_proxy  = undef,
 ) inherits virtualbox::params {
 
-  # this warning is never reached. If Puppet < 4 is used with tis module, Puppet fail with error about data type like "Syntax error at 'String'; expected ')'"
+  # this warning is never reached. If Puppet < 4 is used with tis module,
+  # Puppet fail with error about data type like "Syntax error at 'String'; expected ')'"
   if versioncmp($::puppetversion, '4.0.0') == -1 {
     warning 'Support for Puppet < 3.0 is deprecated. Version 2.0 of this module will only support Puppet >= 4.0' # lint:ignore:80chars
   }
