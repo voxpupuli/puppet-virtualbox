ece127de670dd21600d8e630b54c777d9076df48
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-19 15:22:53 +0200

    Clean up acceptance spec helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,26 +2,16 @@ require 'beaker-rspec'
 require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
-require 'pry'
 
-install_puppet_agent_on(hosts)
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
-    # Install module
-    puppet_module_install(source: proj_root, module_name: 'virtualbox')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-apt'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'stahnma-epel'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'darin-zypprepo'), acceptable_exit_codes: [0, 1]
-    end
+    install_module
+    install_module_dependencies
   end
 end
